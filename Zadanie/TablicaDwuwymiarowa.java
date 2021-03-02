1e471b41beef1f0855bd5ab3b93658c351bc41ae
GokuSunn <78319266+GokuSunn@users.noreply.github.com>
2021-03-02 10:09:17 +0100

    Add files via upload

---
+++ Zadanie/TablicaDwuwymiarowa.java
@@ -0,0 +1,18 @@
+
+
+class TablicaDwuwymiarowa{
+
+    public static void main(String [] args){
+        int [][] tab2d = new int[2][2];        
+        tab2d[0][0] = 1;
+        tab2d[0][1] = 2;
+        tab2d[1][1] = 3;
+        tab2d[1][0] = 4;        
+
+        for (int i=0;i<tab2d.length;i++){
+            for(int j=0;j<tab2d[i].length;j++){
+                System.out.println(" "+tab2d[i][j]);
+            }            
+        }
+    }    
+}
