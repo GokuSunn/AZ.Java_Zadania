9c63e55310934b5626fea1d2e6aa6ea71588eee2
GokuSunn <78319266+GokuSunn@users.noreply.github.com>
2021-04-13 09:52:35 +0200

    Add files via upload

---
+++ 4.13.2021 Obiektowe/Trojkat.java
@@ -0,0 +1,15 @@
+class Trojkat {
+    /*
+    * bylo kilka konstroktowo rozne trojkaty
+    */
+
+    public Trojkat(int wysokosc, int podstawa)
+    {
+        System.out.println(wysokosc*podstawa/2);
+    }
+
+    public Trojkat2(int a,int h)
+    {
+        System.out.println("Pole trojkata: " + (a*h/2));
+    }   
+}
